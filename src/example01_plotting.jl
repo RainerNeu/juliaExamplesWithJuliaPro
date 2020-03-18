d85ab670bdc4bd3c68084255dc03eb3bfa30677b
Rainer Neuberger <neuberger.rainer@gmail.com>
2020-03-18 16:54:41 +0100

    Adding a simple Plotting Example working
    works with JuliaPro

---
+++ src/example01_plotting.jl
@@ -0,0 +1,5 @@
+using Plots;                            #Use the Package Plots
+gr()                                    #Set Backend to gr for the package Plots
+arr=[1,2,3,4,2,7,9,-5,-3,5,5,4];        #Create an Array
+myPlot = plot(arr);                     #Create a Object of the graphic
+display(myPlot);                        #Show plot in Window
